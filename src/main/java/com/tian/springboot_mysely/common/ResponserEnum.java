c507e77ea2feb82f3d6c92f0ca2ef88e41bd7ebb
SmartDragon1225 <2455404279@qq.com>
2021-08-09 20:22:10 +0800

    增加了验证码功能！（主要是验证码工具类）！

--- src/main/java/com/tian/springboot_mysely/common/ResponserEnum.java
+++ src/main/java/com/tian/springboot_mysely/common/ResponserEnum.java
@@ -1,7 +1,6 @@
 package com.tian.springboot_mysely.common;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
--- src/main/java/com/tian/springboot_mysely/controller/LoginController.java
+++ src/main/java/com/tian/springboot_mysely/controller/LoginController.java
@@ -1,7 +1,16 @@
 package com.tian.springboot_mysely.controller;
 
+import com.tian.springboot_mysely.utils.ValidateImageCodeUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * Package: com.tian.springbootlogin.controller
@@ -23,5 +32,18 @@ public class LoginController {
     @RequestMapping("/index")
     public String login1(){
         return "html/login";
+    }
+
+    //生成验证码
+    @GetMapping("/code")
+    public void getVerification(HttpSession session, HttpServletResponse response) throws IOException {
+        //生成验证码
+        String securityCode = ValidateImageCodeUtils.getSecurityCode();
+        BufferedImage image = ValidateImageCodeUtils.createImage(securityCode);
+        //存入session中
+        session.setAttribute("code", securityCode);
+        //响应图片
+        ServletOutputStream os = response.getOutputStream();
+        ImageIO.write(image, "png", os);
     }
 }
